33c949bece6fe1f94439c0836bd538e1481e7684
GraceK505 <antoinefaith1@gmail.com>
2026-06-09 23:19:47 +0200

    deploy 18 commit

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,15 +1,11 @@
 import {NextIntlClientProvider} from 'next-intl';
-import {getMessages} from 'next-intl/server';
 import "../globals.css";
-import Loader from '@/components/loader';
 import MainPage from './page';
-import { redirect } from 'next/navigation';
 
 const loadMessages = async (locale: string) => {
   try {
     return (await import(`../../messages/${locale}.json`)).default;
   } catch {
-    // Fallback to English if the locale file is missing
     return (await import(`../../messages/en.json`)).default;
   }
 };
@@ -21,9 +17,9 @@ export default async function LocaleLayout({
   children: React.ReactNode;
   params: Promise<{ locale: string }>;
 }) {
-   const { locale } = await params;
+  const { locale } = await params;
   const messages = await loadMessages(locale);
-  
+
   return (
     <NextIntlClientProvider messages={messages}>
       <MainPage children={children} />
